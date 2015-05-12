4135f5799f9fa1c10dc7f45a24bb0a2022c02018
Jakob Gaardsted <jgaardsted@gmail.com>
2015-05-12 09:02:22 +0200

    init mmodel works

--- public/javascripts/xp1.js
+++ public/javascripts/xp1.js
@@ -1,64 +1,16 @@
 // Browser-client-javascript-code:
 var socket = io.connect(); // how do we know we have io available?! is it layout.jade load-order?
 
-var clientId = 0; // undefined at start.
-var mobs = {}; //  dirty, fixme (here, so welcome and DCL both can see it.)
-
-var mobColors = [ // 8
-  "#f00",
-  "#0f0",
-  "#00f",
-  "#ff0",
-  "#0ff",
-  "#f0f",
-  "#f80",
-  "#8f0",
-];
-
-socket.on('welcome', function (data_json) {
-  var mobColor;
-  console.log('client-welcome, assigned client ID:', data_json);
-  clientId = data_json.clientId; 
-  mobColor = mobColors[ (clientId%8) ];
-  mobs[clientId] = { 
-    x: 50, 
-    y: 50, 
-    id: clientId,
-    mobColor: mobColor 
-  };
-});
-
 
 document.addEventListener('DOMContentLoaded', function() {
+  console.log('DOMContentLoaded');
 
   var theAts = document.getElementById("atCont");
-  // var theAt = document.getElementById("at");
+  var d = 4; // delta-move-size.
+  var mobElms = {}; // DOM elements.
+  var mobs = {}; // Model objects.
+  var ownClientId = 0; // undefined at start.
 
-  var d = 4;
-  var mobElms = {};
-
-  function procureMob(id) {
-    var ne;
-    var mobColor;
-    // mobElms.hasOwnProperty(id) 
-    if (!(id in mobElms)) {
-      mobColor = mobs[id].mobColor;
-
-      // create it as dom-elm:
-      ne = document.createElement('p');
-      ne.style.color = mobColor;
-
-      $(ne).addClass('atsign')
-           .html('@')
-           .appendTo($('#atCont'))         
-           .attr('id', id);
-
-      mobElms[id] = ne; // store it.
-
-    }
-    return mobElms[id];
-  } // end  procureMob.
-  
   function moveMob(mob_id) {
     var mob = mobs[mob_id];
     var mobElm = procureMob(mob_id);
@@ -68,16 +20,68 @@ document.addEventListener('DOMContentLoaded', function() {
     console.log(mob);
   }
 
+  function procureMob(id) {
+    var newElm; 
+    var mob;
+    // mobElms.hasOwnProperty(id) 
+    if (!(id in mobElms)) {
+      mob = mobs[id];
+      // create it as a DOM-elm:
+      newElm = document.createElement('p');
+      newElm.style.color = mob.mobColor;
+
+      $(newElm).addClass('atsign')
+           .html('@')
+           .appendTo($('#atCont'))         
+           .attr('id', id);
+
+      mobElms[id] = newElm; // store it.
+    }
+    return mobElms[id];
+  } // end  procureMob.
+  
+  function initMob(assignedId) {
+    var mobColors = [ // 8
+      "#f00",
+      "#0f0",
+      "#00f",
+      "#ff0",
+      "#0ff",
+      "#f0f",
+      "#f80",
+      "#8f0",
+    ];
+    var mobColor, mob;
+
+    ownClientId = assignedId;
+    mobColor = mobColors[ (ownClientId%8) ];
+    mob = { 
+      x: 50, 
+      y: 50, 
+      id: ownClientId,
+      mobColor: mobColor 
+    };
+
+    mobs[mob.id] = mob;
+    moveMob(mob.id);
+  }
+
+  socket.on('welcome', function (data_json) {
+    console.log('client-welcome, assigned client ID:', data_json);
+    initMob(data_json.clientId);
+  });
+
   document.addEventListener('keydown', function(e) {
-    var mob = mobs[clientId];
+    var mob = mobs[ownClientId];
+
     switch (e.key) {
     case 'ArrowUp'   : mob.y-=d; break; 
     case 'ArrowDown' : mob.y+=d; break;
     case 'ArrowRight': mob.x+=d; break;
     case 'ArrowLeft':  mob.x-=d; break;
     }
-    moveMob(mob.id);
-    socket.emit('move-c-s', mob);      // ATM necessary, because broadcast doesn't hit yourself..
+    moveMob(mob.id); // ATM necessary, because broadcast doesn't hit yourself..
+    socket.emit('move-c-s', mob);      
   }); // on-keydown.
 
   socket.on('move-s-c', function (mob) {
