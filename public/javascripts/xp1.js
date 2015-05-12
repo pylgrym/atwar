fc49c9526a9343a6fc026057975c338a981bf817
Jakob Gaardsted <jgaardsted@gmail.com>
2015-05-12 08:37:06 +0200

    coloured ats

--- public/javascripts/xp1.js
+++ public/javascripts/xp1.js
@@ -4,41 +4,71 @@ var socket = io.connect(); // how do we know we have io available?! is it layout
 var clientId = 0; // undefined at start.
 var mobs = {}; //  dirty, fixme (here, so welcome and DCL both can see it.)
 
+var mobColors = [ // 8
+  "#f00",
+  "#0f0",
+  "#00f",
+  "#ff0",
+  "#0ff",
+  "#f0f",
+  "#f80",
+  "#8f0",
+];
+
 socket.on('welcome', function (data_json) {
+  var mobColor;
   console.log('client-welcome, assigned client ID:', data_json);
   clientId = data_json.clientId; 
-  mobs[clientId] = { x: 50, y: 50, id: clientId };
+  mobColor = mobColors[ (clientId%8) ];
+  mobs[clientId] = { 
+    x: 50, 
+    y: 50, 
+    id: clientId,
+    mobColor: mobColor 
+  };
 });
 
 
 document.addEventListener('DOMContentLoaded', function() {
 
-  var theAt = document.getElementById("at");
   var theAts = document.getElementById("atCont");
+  // var theAt = document.getElementById("at");
 
   var d = 4;
   var mobElms = {};
 
   function procureMob(id) {
     var ne;
+    var mobColor;
     // mobElms.hasOwnProperty(id) 
     if (!(id in mobElms)) {
+      mobColor = mobs[id].mobColor;
+
       // create it as dom-elm:
       ne = document.createElement('p');
+      ne.style.color = mobColor;
 
       $(ne).addClass('atsign')
-           .html('Z')
+           .html('@')
            .appendTo($('#atCont'))         
            .attr('id', id);
 
       mobElms[id] = ne; // store it.
+
     }
     return mobElms[id];
   } // end  procureMob.
-
+  
+  function moveMob(mob_id) {
+    var mob = mobs[mob_id];
+    var mobElm = procureMob(mob_id);
+    mobElm.style.left = mob.x+"px"; // Update our view.
+    mobElm.style.top  = mob.y+"px"; 
+    // theAt.innerHTML  = mob.id;
+    console.log(mob);
+  }
 
   document.addEventListener('keydown', function(e) {
-
     var mob = mobs[clientId];
     switch (e.key) {
     case 'ArrowUp'   : mob.y-=d; break; 
@@ -46,32 +76,13 @@ document.addEventListener('DOMContentLoaded', function() {
     case 'ArrowRight': mob.x+=d; break;
     case 'ArrowLeft':  mob.x-=d; break;
     }
-
-    // ATM necessary, because broadcast doesn't hit yourself..
-    // No, only move when receiving the move!
-    theAt.style.left = mob.x+"px"; 
-    theAt.style.top  = mob.y+"px"; 
-
-    socket.emit('move-c-s', mob); 
-
-    var mobElm = procureMob(clientId);
-    mobElm.style.left = mob.x+"px"; // Update our view.
-    mobElm.style.top  = mob.y+"px"; 
+    moveMob(mob.id);
+    socket.emit('move-c-s', mob);      // ATM necessary, because broadcast doesn't hit yourself..
   }); // on-keydown.
 
-
   socket.on('move-s-c', function (mob) {
-    var changeId = mob.id;
-    var mobElm = procureMob(changeId);
-    mobs[changeId] = mob; // Transfer to our model-copy.
-
-    console.log(mob);
-    theAt.style.left = mob.x+"px"; // Update our view.
-    theAt.style.top  = mob.y+"px"; 
-    theAt.innerHTML  = mob.id;
-
-    mobElm.style.left = mob.x+"px"; // Update our view.
-    mobElm.style.top  = mob.y+"px"; 
+    mobs[mob.id] = mob; // Transfer to our model-copy
+    moveMob(mob.id);
   });
 
 
