73c05de6561081df6661164749b7d85a658d635a
Jakob Gaardsted <jgaardsted@gmail.com>
2015-05-18 14:12:49 +0200

    removed DOM-multimob

--- public/javascripts/xp1.js
+++ public/javascripts/xp1.js
@@ -4,20 +4,20 @@ var socket;
 document.addEventListener('DOMContentLoaded', function() {
   console.log('DOMContentLoaded');
 
-  var d = 4; // delta-move-size.
-  var mobElms = {}; // DOM elements.
-  var mobs = {}; // Model objects. // PROBLEM: Must go to shared module.
+  //var d = 4; // delta-move-size.
+  //var mobElms = {}; // DOM elements.
+  //var mobs = {}; // Model objects. // PROBLEM: Must go to shared module.
   var ownClientId = 0; // undefined at start.
 
   socket = io.connect(); // how do we know we have io available?! is it layout.jade load-order?
 
-  function moveMob(mob_id) {
-    var mob = mobs[mob_id];
-    var mobElm = procureMob(mob_id);
-    mobElm.style.left = mob.x+"px"; // Update our view.
-    mobElm.style.top  = mob.y+"px"; 
-    console.log(mob);   // theAt.innerHTML  = mob.id;
-  }
+  // function moveMob(mob_id) {
+  //   var mob = mobs[mob_id];
+  //   var mobElm = procureMob(mob_id);
+  //   mobElm.style.left = mob.x+"px"; // Update our view.
+  //   mobElm.style.top  = mob.y+"px"; 
+  //   console.log(mob);   // theAt.innerHTML  = mob.id;
+  // }
 
   function updateMobPos(mob_id, oldX, oldY) {  // NEW behaviour:
     var mob2 = Mob.at(mob_id);
@@ -26,51 +26,51 @@ document.addEventListener('DOMContentLoaded', function() {
     Map.map2screenB(mob2.x, mob2.y);    
   }
 
-  function procureMob(id) {
-    var newElm; 
-    var mob;
-    // mobElms.hasOwnProperty(id) 
-    if (!(id in mobElms)) {
-      mob = mobs[id];
-      // create it as a DOM-elm:
-      newElm = document.createElement('p');
-      newElm.style.color = mob.mobColor;
+  // function procureMob(id) {
+  //   var newElm; 
+  //   var mob;
+  //   // mobElms.hasOwnProperty(id) 
+  //   if (!(id in mobElms)) {
+  //     mob = mobs[id];
+  //     // create it as a DOM-elm:
+  //     newElm = document.createElement('p');
+  //     newElm.style.color = mob.mobColor;
 
-      $(newElm).addClass('atsign')
-           .html('@')
-           .appendTo($('#atCont'))         
-           .attr('id', id);
+  //     $(newElm).addClass('atsign')
+  //          .html('@')
+  //          .appendTo($('#atCont'))         
+  //          .attr('id', id);
 
-      mobElms[id] = newElm; // store it.
-    }
-    return mobElms[id];
-  } // end  procureMob.
+  //     mobElms[id] = newElm; // store it.
+  //   }
+  //   return mobElms[id];
+  // } // end  procureMob.
   
-  function initMob(assignedId) {
-    var mobColors = [ // 8
-      "#f00",
-      "#0f0",
-      "#00f",
-      "#ff0",
-      "#0ff",
-      "#f0f",
-      "#f80",
-      "#8f0",
-    ];
-    var mobColor, mob;
+  // function initMob(assignedId) {
+  //   var mobColors = [ // 8
+  //     "#f00",
+  //     "#0f0",
+  //     "#00f",
+  //     "#ff0",
+  //     "#0ff",
+  //     "#f0f",
+  //     "#f80",
+  //     "#8f0",
+  //   ];
+  //   var mobColor, mob;
 
-    ownClientId = assignedId;
-    mobColor = mobColors[ (ownClientId%8) ];
-    mob = { 
-      x: 10, 
-      y: 10, 
-      id: ownClientId,
-      mobColor: mobColor      
-    };
+  //   ownClientId = assignedId;
+  //   mobColor = mobColors[ (ownClientId%8) ];
+  //   mob = { 
+  //     x: 10, 
+  //     y: 10, 
+  //     id: ownClientId,
+  //     mobColor: mobColor      
+  //   };
 
-    mobs[mob.id] = mob;
-    moveMob(mob.id);
-  }
+  //   mobs[mob.id] = mob;
+  //   moveMob(mob.id);
+  // }
 
   /* TODO/FIXME/NEXT STEPS So, what did I do.. I added module 'Mob', with map/array,
   and 'at(ix)' accessor.
@@ -93,7 +93,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
   socket.on('welcome', function (data_json) {
     console.log('client-welcome, assigned client ID:', data_json);
-    initMob(data_json.clientId); 
+    // initMob(data_json.clientId); 
     Mob.initMob(data_json.clientId);
     // problem - this should give me ALL mobs, instead of just own mob.
     // (we need own id as well as info on all other mobs.)
@@ -101,31 +101,33 @@ document.addEventListener('DOMContentLoaded', function() {
 
   socket.on('move-s-c', function (mob) {
     var oldPos = Mob.at(mob.id);
-    mobs[mob.id] = mob;     // Transfer to our model-copy
+    // mobs[mob.id] = mob;     // Transfer to our model-copy
     Mob.mobs[mob.id] = mob; // Transfer to our model-copy
-    moveMob(mob.id); // JG: Consider: instead of this 'wholesale transfer', could we just transfer coords of foreign mob?
+    // moveMob(mob.id); // JG: Consider: instead of this 'wholesale transfer', could we just transfer coords of foreign mob?
     updateMobPos(mob.id, oldPos.x, oldPos.y);
   });
 
   document.addEventListener('keydown', function(e) {
-    var mob = mobs[ownClientId];
+    // var mob = mobs[ownClientId];
     var mob2 = Mob.at(ownClientId);
     var oldX = mob2.x, oldY = mob2.y;
     switch (e.keyCode) { // Chrome..? Also works in FF.
-    case 37: mob.x-=d; mob2.x -=1; break; // 'ArrowLeft'
-    case 38: mob.y-=d; mob2.y -=1; break; // 'ArrowUp'  
-    case 39: mob.x+=d; mob2.x +=1; break; // 'ArrowRight'
-    case 40: mob.y+=d; mob2.y +=1; break; // 'ArrowDown'
+    case 37: mob2.x -=1; break; // 'ArrowLeft'
+    case 38: mob2.y -=1; break; // 'ArrowUp'  
+    case 39: mob2.x +=1; break; // 'ArrowRight'
+    case 40: mob2.y +=1; break; // 'ArrowDown'
     }
-    moveMob(mob.id); // ATM necessary, because broadcast doesn't hit yourself..
+    // moveMob(mob.id); // ATM necessary, because broadcast doesn't hit yourself..
     updateMobPos(mob2.id, oldX, oldY);
-    socket.emit('move-c-s', mob);      
+    socket.emit('move-c-s', mob2);      
   }); // on-keydown.
 
 }); // on-DCL.
 
 
-
+/*Hmm - i actually had 2/3 different 'mob pos' systems: coords, DOM-multi, and model-multi.
+I still need to get 'coords' out of map!
+*/
 
 
 
